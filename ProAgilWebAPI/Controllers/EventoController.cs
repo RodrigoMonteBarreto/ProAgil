029b65e2a4a070c25f6973d1766cf29376bf07e9
Rodrigo <rodrigobarreto.m@gmail.com>
2021-09-16 15:06:02 -0300

    Projetos Criados

---
+++ ProAgilWebAPI/Controllers/EventoController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgilWebAPI.Data;
+using ProAgilWebAPI.Models;
+
+namespace ProAgilWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var result = await _context.Eventos.ToListAsync();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("Erro");
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var result = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+
+                return BadRequest("Erro");
+            }
+        }
+    }
+}
