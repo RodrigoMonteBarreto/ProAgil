98fe76a1160355793a24246f4547052d6113a5d4
Rodrigo <rodrigobarreto.m@gmail.com>
2021-09-16 16:39:24 -0300

    angular, components, cors, bootstrap, fontanwense

--- ProAgilWebAPI/Startup.cs
+++ ProAgilWebAPI/Startup.cs
@@ -34,6 +34,7 @@ namespace ProAgilWebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProAgilWebAPI", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +53,7 @@ namespace ProAgilWebAPI
 
             app.UseAuthorization();
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
